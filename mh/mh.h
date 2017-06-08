gnu-mirror-unofficial/mailutils

commit d30e5a07f35c756df64559fddcd69d8aaa23ab8d

mh whatnow: allow for arguments in the editor profile value
* mh/mh_whatnow.c (edit): Make sure ed is always initialized.
Accept command line arguments in the "editor" and "*-next" values.

mh/mh_whatnow.c
@@ -382,9 +382,8 @@ display (struct mh_whatnow_env *wh, int argc, char **argv, int *status)
 static int
 edit (struct mh_whatnow_env *wh, int argc, char **argv, int *whs)
 {
-  char const *ed;
+  char const *ed = wh->editor;
   int i, rc, status;
-  char **xargv;
   
   if (wh->reedit)
     {
@@ -403,19 +402,50 @@ edit (struct mh_whatnow_env *wh, int argc, char **argv, int *whs)
     }
   else if (argc > 1)
     ed = argv[1];
+
+  if (argc > 1)
+    {
+      char **xargv;
+
+      xargv = mu_calloc (argc+2, sizeof (*xargv));
+      xargv[0] = (char *)ed;
+      for (i = 1; i + 1 < argc; i++)
+	xargv[i] = argv[i+1];
+      xargv[i++] = wh->file;
+      xargv[i] = NULL;
+      rc = mu_spawnvp (xargv[0], xargv, &status);
+      free (xargv);
+    }
   else
-    ed = wh->editor;
-
-  xargv = mu_calloc (argc+2, sizeof (*xargv));
-  xargv[0] = (char *)ed;
-  for (i = 1; i + 1 < argc; i++)
-    xargv[i] = argv[i+1];
-  xargv[i++] = wh->file;
-  xargv[i] = NULL;
-
-  rc = mu_spawnvp (xargv[0], xargv, &status);
-  free (xargv);
-
+    {      
+      struct mu_wordsplit ws;
+      extern char **environ;
+      if (mu_wordsplit (ed, &ws,
+			MU_WRDSF_QUOTE
+			| MU_WRDSF_SQUEEZE_DELIMS
+			| MU_WRDSF_ENV))
+	{
+	  mu_error (_("cannot split line `%s': %s"), ed,
+		    mu_wordsplit_strerror (&ws));
+	  rc = MU_ERR_FAILURE;
+	}
+      else
+	{
+	  char *xargv[2];
+	  xargv[0] = wh->file;
+	  xargv[1] = NULL;
+	  if (mu_wordsplit_append (&ws, 1, xargv))
+	    {
+	      mu_error (_("cannot append arguments: %s"),
+			mu_wordsplit_strerror (&ws));
+	      rc = ENOMEM;
+	    }
+	  else
+	    rc = mu_spawnvp (ws.ws_wordv[0], ws.ws_wordv, &status);
+	  mu_wordsplit_free (&ws);
+	}
+    }
+      
   if (rc || check_exit_status (ed, status))
     {
       if (wh->file)
@@ -613,8 +643,12 @@ mh_whatnow (struct mh_whatnow_env *wh, int initial_edit)
 {
   set_default_editor (wh);
   
-  if (initial_edit)
-    mh_spawnp (wh->editor, wh->file);
+  if (initial_edit && wh->file)
+    {
+      char *argv[2] = { "edit", NULL };
+      int status;
+      edit (wh, 1, argv, &status);
+    }
 
   if (!wh->prompt)
     wh->prompt = (char*) _("What now?");

commit 766dd82f18c41f4fbf403e6627cb9a37a8b8a69f

Re-use last invoked editor in MH whatnow
This fixes a bug introduced in d30e5a07

* mh/mh.h (mh_whatnow_env) <last_ed>: New member
* mh/mh_whatnow.c (_whatnow): Initialize reedit and last_ed members
Free last_ed before returning
(edit): Prefer wh->last_ed over wh->editor

mh/mh.h
@@ -210,13 +210,14 @@ struct mh_whatnow_env     /* whatnow shell environment */
   char *file;             /* The file being processed */
   char *msg;              /* File name of the original message (if any) */
   char *draftfile;        /* File to preserve the draft into */
-  const char *editor;
+  const char *editor;     /* Default editor */
   char *prompt;
   char *anno_field;       /* Annotate field to be used */
   mu_list_t anno_list;    /* List of messages (mu_message_t) to annotate */
   mu_mailbox_t mbox;
   int nowhatnowproc;
   int reedit:1;           /* Set if the editor was already invoked */
+  char *last_ed;          /* Last used editor */
 };
 
 #define DISP_QUIT 0

mh/mh_whatnow.c
@@ -323,6 +323,9 @@ _whatnow (struct mh_whatnow_env *wh, struct action_tab *tab)
   size_t size = 0;
   struct mu_wordsplit ws;
   int wsflags = MU_WRDSF_DEFFLAGS|MU_WRDSF_COMMENT;
+
+  wh->reedit = 0;
+  wh->last_ed = NULL;
   
   do
     {
@@ -360,6 +363,8 @@ _whatnow (struct mh_whatnow_env *wh, struct action_tab *tab)
   while (rc == 0);
   if (wsflags & MU_WRDSF_REUSE)
     mu_wordsplit_free (&ws);
+  free (wh->last_ed);
+  wh->last_ed = NULL;
   free (line);
   return status;
 }
@@ -382,8 +387,9 @@ display (struct mh_whatnow_env *wh, int argc, char **argv, int *status)
 static int
 edit (struct mh_whatnow_env *wh, int argc, char **argv, int *whs)
 {
-  char const *ed = wh->editor;
+  char const *ed = wh->last_ed ? wh->last_ed : wh->editor;
   int i, rc, status;
+  char *p;
   
   if (wh->reedit)
     {
@@ -455,8 +461,10 @@ edit (struct mh_whatnow_env *wh, int argc, char **argv, int *whs)
       else
 	mu_error (_("problems with edit"));
     }
-  
-  wh->editor = ed;
+
+  p = mu_strdup (ed);
+  free (wh->last_ed);
+  wh->last_ed = p;
   wh->reedit = 1;
   
   return 0;