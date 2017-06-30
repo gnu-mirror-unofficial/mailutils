10b7d2748c77205579a5ca424e4edf8770f660b6
Sergey Poznyakoff <gray@gnu.org.ua>
2017-06-30 10:22:45 +0300

    Restore version output expected by mh-e
    * mh/mh_getopt.c (mh_version_hook): Print version in format expected
    by mh-e
    (process_std_options): Produce usual version output when responding
    to --version

--- mh/mh_getopt.c
+++ mh/mh_getopt.c
@@ -92,7 +92,7 @@ process_std_options (int argc, char **argv, struct mu_parseopt *po)
     }
   if (strcmp (argv[0], "--version") == 0)
     {
-      mu_program_version (po, mu_strout);
+      mu_version_hook (po, mu_strout);
       exit (0);
     }
 }
@@ -145,14 +145,13 @@ static struct mu_option folder_option[] = {
 void
 mh_version_hook (struct mu_parseopt *po, mu_stream_t stream)
 {
+  mu_stream_printf (stream, "%s (%s %s)\n", mu_program_name,
+		    PACKAGE_NAME, PACKAGE_VERSION);
 #if MU_GIT_COMMIT_DISTANCE > 0  
-  mu_stream_printf (stream, "%s (%s) %s-%d [%s]\n",
-		    mu_program_name, PACKAGE_NAME, PACKAGE_VERSION,
+  mu_stream_printf (stream, "version: %s %s-%d [%s]\n",
+		    PACKAGE_NAME, PACKAGE_VERSION,
 		    MU_GIT_COMMIT_DISTANCE,
 		    MU_GIT_DESCRIBE_STRING);
-#else
-  mu_stream_printf (stream, "%s (%s) %s\n", mu_program_name,
-		    PACKAGE_NAME, PACKAGE_VERSION);
 #endif
   /* TRANSLATORS: Translate "(C)" to the copyright symbol
      (C-in-a-circle), if this symbol is available in the user's
